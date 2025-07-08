5d45eaeac6d0e93446831c1367a0f2cb1e452dbb
Bhavankumarg <gbhavanbkr@gmail.com>
2025-07-08 12:39:13 +0530

    text size for mobile changed on tech/it page

--- components/IndustriesComponents/TechIT/CaseStudiesCard.js
+++ components/IndustriesComponents/TechIT/CaseStudiesCard.js
@@ -8,14 +8,14 @@ const CaseStudiesCard = () => {
     <>
       <div>
         <div className="container mx-auto flex flex-col items-center justify-center">
-          <h3 className="lg:text-[32px] text-xl text-center lg:py-10 lg:p-0 p-5">
+          <h3 className="lg:text-[32px] text-xl text-center lg:py-10 lg:p-0 p-5 ">
             Smarter Lead Generation + Scalable Digital Execution for Tech
             Companies
           </h3>
           <Link href="/b2b-lead-generation-for-tech" className="border iv-bg text-white p-3 rounded-lg mb-5 lg:mb-10 text-2xl">
             Explore More
           </Link>
-          <h3 className="lg:text-[22px] text-xl text-center lg:pt-12 lg:pb-0 pb-5 pt-5">
+          <h3 className="lg:text-[22px] text-md text-center lg:pt-12 lg:pb-0 pb-5 pt-5">
             Here are some instances where our expertise and cutting-edge
             solutions have propelled Tech/IT companies to success
           </h3>
