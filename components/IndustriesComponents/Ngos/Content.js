c416fba09210877cd6c7ed0e265cadb80dbeec8d
Bhavankumarg <gbhavanbkr@gmail.com>
2025-07-11 11:47:16 +0530

    nogs title casing have been updated

--- components/IndustriesComponents/Ngos/Content.js
+++ components/IndustriesComponents/Ngos/Content.js
@@ -6,8 +6,8 @@ const Content = () => {
     <>
       <div>
         <div className="container mx-auto">
-          <h1 className="text-[40px] text-[#ff0000] uppercase font-monument-bold text-center lg:pt-10 pt-5">
-            NOT-FOR-PROFITS/ NGOs
+          <h1 className="text-[40px] text-[#ff0000] font-monument-bold text-center lg:pt-10 pt-5">
+            <span className="uppercase font-monument-bold">NOT-FOR-PROFITS/ NGO</span>s
           </h1>
           <p className="text-center lg:text-xl font-monument-bold lg:py-8 p-5">
             As social development produces both new possibilities and fresh
