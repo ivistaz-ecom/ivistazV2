8002b8c0f40b7ff5fcc30cd5c73e27b19142317d
Bhavankumarg <gbhavanbkr@gmail.com>
2025-07-28 17:10:54 +0530

    tech landing page banner video has fixed/checking

--- components/GenerationForTech/Banner.js
+++ components/GenerationForTech/Banner.js
@@ -11,8 +11,10 @@ const Banner = () => {
           loop
           muted
           playsInline
+          poster="/generation-for-tech/blue-bg-poster.jpg" // fallback image for fast loading
         >
-          <source src="/generation-for-tech/blue-bg.webm" type="video/mp4" />
+          <source src="/generation-for-tech/blue-bg.mp4" type="video/mp4" />
+          <source src="/generation-for-tech/blue-bg.webm" type="video/webm" />
           Your browser does not support the video tag.
         </video>
 
@@ -21,14 +23,12 @@ const Banner = () => {
 
         {/* Content */}
         <div className="relative z-10 text-white flex flex-col items-center justify-center h-full text-center">
-          <h1 className="text-3xl md:text-[75px] ">
-            Smarter Lead Generation <span className="text-[#FF5C00] font-monument-semibold"> +</span>{" "}
+          <h1 className="text-3xl md:text-[75px]">
+            Smarter Lead Generation <span className="text-[#FF5C00] font-monument-semibold"> +</span>
           </h1>
-          <h1 className="text-3xl md:text-[80px]">
-            Scalable Digital Execution{" "}
-          </h1>
-          <h1 className="text-3xl md:text-[80px] font- mb-4">
-            for <span className="text-[#FF5C00] font-"> Tech Companies</span>
+          <h1 className="text-3xl md:text-[80px]">Scalable Digital Execution</h1>
+          <h1 className="text-3xl md:text-[80px] mb-4">
+            for <span className="text-[#FF5C00]">Tech Companies</span>
           </h1>
         </div>
       </div>
