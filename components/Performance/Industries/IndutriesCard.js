b118540dd44d5d58408b3a0d124ef9c22564fe49
Bhavankumarg <gbhavanbkr@gmail.com>
2025-08-04 12:38:58 +0530

    animated background - line issue have been fixed

--- components/Performance/Industries/IndutriesCard.js
+++ components/Performance/Industries/IndutriesCard.js
@@ -12,10 +12,10 @@ const IndutriesCard = () => {
           {IndutriesCardData.map((item, index) => (
             <div key={index} className="group">
               <div className="pb-2 overflow-hidden">
-                <Image 
-                  src={item.imgUrl} 
-                  alt="icon" 
-                  width={600} 
+                <Image
+                  src={item.imgUrl}
+                  alt="icon"
+                  width={600}
                   height={600}
                   className="transition-transform duration-500 ease-in-out group-hover:scale-110 w-[800px]"
                 />
@@ -25,8 +25,8 @@ const IndutriesCard = () => {
                 className="group relative flex items-center gap-2 justify-between py-3 px-5 overflow-hidden transition-all duration-300 bg-gray-100 group-hover:border-l-0"
               >
                 {/* Animated background */}
-                <div className="absolute top-0 left-0 right-0 bottom-0 bg-black transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out pointer-events-none"></div>
-                
+                <div className="absolute top-0 -left-1 right-0 bottom-0 bg-black transform -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out pointer-events-none"></div>
+
                 {/* Content */}
                 <div className="relative z-10 flex items-center gap-2 justify-between w-full">
                   <h4 className="text-[20px] font-monument-medium text-start text-black group-hover:text-white transition-colors duration-300">
@@ -35,7 +35,6 @@ const IndutriesCard = () => {
                   <FaChevronRight className="text-2xl text-black group-hover:text-white lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-300" />
                 </div>
               </Link>
-              
             </div>
           ))}
         </div>
